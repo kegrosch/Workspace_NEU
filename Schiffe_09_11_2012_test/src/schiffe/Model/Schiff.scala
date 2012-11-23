d33d911f3f4dece14e3f5ebc1b40816ccd82d117
Kevin Grosch <kegrosch@htwg-konstanz.de>
2012-11-23 16:21:07 +0100

    weitere Umgebungstests implementiert
    Umgebungstest fertiggestellt: 11/24

--- Schiffe_09_11_2012_test/src/schiffe/Model/Schiff.scala
+++ Schiffe_09_11_2012_test/src/schiffe/Model/Schiff.scala
@@ -22,11 +22,16 @@ class Schiff(var laenge: Int, var startZelle: Zelle, var zellen: Array[Array[Zel
             if ((((startZelle.reihe) - laenge) + 1) >= 0) {
 
               if (schiffKontrolleOben == true) {
+                 if(umfeldKontrolle(groesse, direction)==true){
                 for (i <- 0 to laenge - 1) {
                   zellen((startZelle.reihe - i))(startZelle.spalte).setzen(true)
 
                 }
                 return true
+                }else {
+                gesetzt = false
+                zaehler = zaehler + 1
+              }
               } else {
                 gesetzt = false
                 zaehler = zaehler + 1
@@ -86,11 +91,18 @@ class Schiff(var laenge: Int, var startZelle: Zelle, var zellen: Array[Array[Zel
             if ((((startZelle.spalte) + laenge) - 1) <= groesse) {
 
               if (schiffKontrolleRechts == true) {
+                if(umfeldKontrolle(groesse, direction)==true){
+                  
+                
                 for (i <- 0 to laenge - 1) {
                   zellen((startZelle.reihe))(startZelle.spalte + i).setzen(true)
 
                 }
                 return true
+                }else {
+                gesetzt = false
+                zaehler = zaehler + 1
+              }
               } else {
                 gesetzt = false
                 zaehler = zaehler + 1
@@ -113,11 +125,16 @@ class Schiff(var laenge: Int, var startZelle: Zelle, var zellen: Array[Array[Zel
           } else {
             if ((((startZelle.spalte) - laenge) + 1) >= 0) {
               if (schiffKontrolleLinks == true) {
+                if(umfeldKontrolle(groesse, direction)==true){
                 for (i <- 0 to laenge - 1) {
                   zellen((startZelle.reihe))(startZelle.spalte - i).setzen(true)
 
                 }
                 return true
+                }else {
+                gesetzt = false
+                zaehler = zaehler + 1
+              }
               } else {
                 gesetzt = false
                 zaehler = zaehler + 1
@@ -363,7 +380,38 @@ class Schiff(var laenge: Int, var startZelle: Zelle, var zellen: Array[Array[Zel
                   }
                 }
               case 3 => // Schiff geht links
-                //überprüfen ob links anstößt
+                 if(0 == (startZelle.getSpalte-laenge+1)){//überprüfen ob links anstößt
+                  //Schiff stößt links an
+                  for(i <- (startZelle.getSpalte-1) to groesse){
+                    if((zellen(startZelle.getReihe+1)(i).getGesetzt)==true){
+                      return false
+                    }else{
+                      okay = true
+                    }
+                  }
+                  if((zellen(startZelle.getReihe)(startZelle.getSpalte-1).getGesetzt)==true){
+                    return false
+                  }else{
+                    return okay
+                  }
+                  
+                }else{//Schiff stößt nicht rechts an
+                for(i <- (startZelle.getSpalte-1) to (startZelle.getSpalte+laenge)){
+                  if((zellen(startZelle.getReihe+1)(i).getGesetzt)==true){
+                      return false
+                    }else{
+                      okay = true
+                    }
+                }
+                if((zellen(startZelle.getReihe)(startZelle.getSpalte-1).getGesetzt)==true){
+                    return false
+                  }else{
+                    if((zellen(startZelle.getReihe)(startZelle.getSpalte+1).getGesetzt)==true){
+                    return okay
+                    }
+                  }
+                }
+                
             }
         }
       case `groesse` => // Reihe = letzte Reihe
@@ -371,12 +419,70 @@ class Schiff(var laenge: Int, var startZelle: Zelle, var zellen: Array[Array[Zel
           case 0 => //Spalte = erste Spalte & Reihe = letzte Reihe
             richtung match {
               case 0 => //Schiff geht nach oben
+                for (i <- groesse to (startZelle.getReihe-laenge)) {
+                  if((zellen(i)(startZelle.getSpalte+1).getGesetzt)==true){
+                    return false
+                  }else{
+                    okay = true
+                  }
+                }
+                if(((zellen(startZelle.getReihe-laenge)(startZelle.getSpalte).getGesetzt)==true)){
+                  return false
+                }else{
+                  
+                    return okay
+                  
+                }
               case 2 => //Schiff geht nach rechts
+                for (i <- 0 to laenge) {
+                   if((zellen(startZelle.getReihe-1)(startZelle.getSpalte+i).getGesetzt)==true){
+                    return false
+                  }else{
+                    okay = true
+                  }
+                 
+                 }
+                 if(((zellen(startZelle.getReihe)(startZelle.getSpalte+laenge).getGesetzt)==true)){
+                  return false
+                }else{
+                  
+                    return okay
+                  
+                }
             }
           case `groesse` => //Spalte = letzte Spalte & Reihe = letzte Reihe
             richtung match {
               case 0 => //Schiff geht nach oben
+                for (i <- groesse to (startZelle.getReihe-laenge)) {
+                  if((zellen(i)(startZelle.getSpalte-1).getGesetzt)==true){
+                    return false
+                  }else{
+                    okay = true
+                  }
+                }
+                if(((zellen(startZelle.getReihe-laenge)(startZelle.getSpalte).getGesetzt)==true)){
+                  return false
+                }else{
+                  
+                    return okay
+                  
+                }
               case 3 => //Schiff geht nach links
+                for (i <- groesse to (groesse-laenge)) {
+                   if((zellen(startZelle.getReihe-1)(startZelle.getSpalte-i).getGesetzt)==true){
+                    return false
+                  }else{
+                    okay = true
+                  }
+                 
+                 }
+                 if(((zellen(startZelle.getReihe)(startZelle.getSpalte-laenge).getGesetzt)==true)){
+                  return false
+                }else{
+                  
+                    return okay
+                  
+                }
             }
           case _ => //Spalte != erste Spalte & Spalte != letzte Spalte & Reihe = letzte Reihe
             richtung match {
