df57d746b6f5920ef9798f7a8a8448dfdad28d74
Kevin Grosch <kegrosch@htwg-konstanz.de>
2012-11-27 17:38:30 +0100

    Test für schiff geändert

--- Schiffe_09_11_2012_test/test/schiffe/Model/SchiffSpec.scala
+++ Schiffe_09_11_2012_test/test/schiffe/Model/SchiffSpec.scala
@@ -6,8 +6,11 @@ import org.specs2.runner.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class SchiffSpec extends SpecificationWithJUnit{
-  "A new Feld" should{
+  "A new Schiff" should{
     val feld = new Feld(2)
+    val zellen = feld.zellen
+    val startZelle = new Zelle(0,0)
+    val schiff = new Schiff(2, startZelle, zellen)
     
     "Have 2 cells" in{
       var anzahl = feld.zellen.length
