c25024cc67f38a3171e958703488c48486eadb57
Daryna <darynka@mail.ua>
2013-01-01 11:56:24 +0100

    var Schiffegesetzt PCPanel

--- Schiffe_09_11_2012_test/src/schiffe/View/PCPanel.scala
+++ Schiffe_09_11_2012_test/src/schiffe/View/PCPanel.scala
@@ -12,7 +12,7 @@ val Schiffgetroffen = new Color(190, 245, 170)
 val SchiffNichtgetroffen = new Color(150, 160, 162)
 var buttons = Array.ofDim[Button](size, size)
 def getZelle(reihe: Int, spalte: Int): Zelle = pccontroller.cell(reihe, spalte)
-
+var schiffegesetzt =false
 
 
 def spielfeld(size: Int, spielSize: Int) = new GridPanel(spielSize, spielSize){
