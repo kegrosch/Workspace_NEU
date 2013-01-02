c7561832d9eec34b19fb3bed7434389b07d96d54
Kevin Grosch <kegrosch@htwg-konstanz.de>
2013-01-02 14:36:38 +0100

    Kommentar eingefügt

--- Schiffe_09_11_2012_test/src/schiffe/View/SpielerPanel.scala
+++ Schiffe_09_11_2012_test/src/schiffe/View/SpielerPanel.scala
@@ -19,7 +19,7 @@ class SpielerPanel(controller: Controller, size: Int, schiffPanel: SchiffPanel) 
   var startReihe = 0
   var startSpalte = 0
   var richtung = 0
-  
+  //TEST
   val InitialfarbeSpieler = new Color(200, 200, 255)
   val Schiffgesetzt = new Color(192, 255, 192)
   val Schiffgetroffen = new Color(190, 245, 170)
