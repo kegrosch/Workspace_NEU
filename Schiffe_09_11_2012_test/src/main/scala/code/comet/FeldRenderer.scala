ec4ba897c9449559a10f05e1efef71d3267538cb
Kevin Grosch <kegrosch@htwg-konstanz.de>
2013-01-05 23:00:10 +0100

    LIFT EINSTELLUNGEN

---
+++ Schiffe_09_11_2012_test/src/main/scala/code/comet/FeldRenderer.scala
@@ -0,0 +1,56 @@
+package code
+package comet
+
+import net.liftweb._
+import http._
+import util._
+import Helpers._
+
+import scala.swing.Reactor
+
+import schiffe.Schiffe
+import schiffe.Controller._
+import schiffe.Controller.CellChanged
+
+/**
+ * The screen real estate on the browser will be represented
+ * by this component.  When the component changes on the server
+ * the changes are automatically reflected in the browser.
+ */
+class FeldRenderer extends CometActor with CometListener with Reactor{
+
+  listenTo(Schiffe.controller)
+
+  reactions += {
+    //case e: GridSizeChanged => resize(e.newSize)
+    
+  
+  case e: CellChanged => reRender()
+  }
+  private var status: String = "" // private state
+
+  /**
+   * When the component is instantiated, register as
+   * a listener with the ChatServer
+   */
+  def registerWith = SchiffeServer
+
+  /**
+   * The CometActor is an Actor, so it processes messages.
+   * In this case, we're listening for Vector[String],
+   * and when we get one, update our private state
+   * and reRender() the component.  reRender() will
+   * cause changes to be sent to the browser.
+   */
+  override def lowPriority = {
+    case command: String => reRender()
+  }
+
+  /**
+   * Put the messages in the li elements and clear
+   * any elements that have the clearable class.
+   */
+  def render = {
+    "#status *" #> status
+  }
+}
--- Schiffe_09_11_2012_test/src/main/scala/schiffe/Schiffe.scala
+++ Schiffe_09_11_2012_test/src/main/scala/schiffe/Schiffe.scala
@@ -5,12 +5,13 @@ import schiffe.Controller._
 import schiffe.Model._
 
 object Schiffe {
-  //MAIN KLASSE
-  def main(args: Array[String]) {
-    val controller = new Controller(new Feld(5))
+  val controller = new Controller(new Feld(5))
     val pccontroller = new Controller(new Feld(5))
     val userTui=new Tui(controller, pccontroller)
     val userGui = new GUI(controller, pccontroller)
+  //MAIN KLASSE
+  def main(args: Array[String]) {
+    
 //    userGui.spiel(5)
     userGui.visible = true
     
