f433b66c20be2474c9f7cfdd053e2df49142f3b6
Kevin Grosch <kegrosch@htwg-konstanz.de>
2013-01-17 20:42:08 +0100

    Lift schiffe setzen form invisible

--- Schiffe_09_11_2012_test/src/main/scala/code/snippet/SchiffAuswahl.scala
+++ Schiffe_09_11_2012_test/src/main/scala/code/snippet/SchiffAuswahl.scala
@@ -7,6 +7,8 @@ import code.comet.SchiffeServer
 import scala.xml.NodeSeq
 import schiffe.Schiffe
 import net.liftweb.http.S
+import net.liftweb.http.js._
+import JsCmds._
 
 
 object SchiffAuswahl {
@@ -25,13 +27,102 @@ object SchiffAuswahl {
   var k2 = false
   var s = false
   
+  def isSet ={
+    Schiffe.controller.getSize match{
+      case 2 =>{
+         if(z_klein==true){
+      <input type="hidden"/>
+    }else{
+      <form id="form2" class="lift:SchiffSetzen?form=post">
+							<select name="typ">
+								<option name="typ" value="set">Schiffe setzen</option>
+								<option name="typ" value="hit">Schiessen</option>
+							</select>
+					
+						
+						<select name="schiff"><option class="lift:SchiffAuswahl.schiffe"></option></select>
+
+					
+						<select name="reihe"><option class="lift:SchiffAuswahl.startButtonReihe"></option></select>
+					
+						<select name="spalte"> <option class="lift:SchiffAuswahl.startButtonSpalte"></option></select>
+					<select name="richtung">
+							<option>rechts</option>
+							<option>links</option>
+							<option>oben</option>
+							<option>unten</option>
+					</select>
+					<input type="submit" value="Schiff setzen"/>
+					</form>
+    }
+        
+      }
+      case 5 =>{
+         if(z_mittel==true && u_mittel==true && k_mittel==true){
+      <input type="hidden"/>
+    }else{
+      <form id="form2" class="lift:SchiffSetzen?form=post">
+							<select name="typ">
+								<option name="typ" value="set">Schiffe setzen</option>
+								<option name="typ" value="hit">Schiessen</option>
+							</select>
+					
+						
+						<select name="schiff"><option class="lift:SchiffAuswahl.schiffe"></option></select>
+
+					
+						<select name="reihe"><option class="lift:SchiffAuswahl.startButtonReihe"></option></select>
+					
+						<select name="spalte"> <option class="lift:SchiffAuswahl.startButtonSpalte"></option></select>
+					<select name="richtung">
+							<option>rechts</option>
+							<option>links</option>
+							<option>oben</option>
+							<option>unten</option>
+					</select>
+					<input type="submit" value="Schiff setzen"/>
+					</form>
+    }
+        
+      }
+      case 10 =>{
+         if(z1==true && z2==true && z4==true && u1==true && u2==true && u3==true &&k1==true && k2==true && s==true){
+      <input type="hidden"/>
+    }else{
+      <form id="form2" class="lift:SchiffSetzen?form=post">
+							<select name="typ">
+								<option name="typ" value="set">Schiffe setzen</option>
+								<option name="typ" value="hit">Schiessen</option>
+							</select>
+					
+						
+						<select name="schiff"><option class="lift:SchiffAuswahl.schiffe"></option></select>
+
+					
+						<select name="reihe"><option class="lift:SchiffAuswahl.startButtonReihe"></option></select>
+					
+						<select name="spalte"> <option class="lift:SchiffAuswahl.startButtonSpalte"></option></select>
+					<select name="richtung">
+							<option>rechts</option>
+							<option>links</option>
+							<option>oben</option>
+							<option>unten</option>
+					</select>
+					<input type="submit" value="Schiff setzen"/>
+					</form>
+    }
+        
+      }
+    }
+   
+  }
   def schiffe(in: NodeSeq):NodeSeq = {
     Schiffe.controller.getSize match{
-      case 2 => if(z_klein==false){<option value="Zerstörer">Zerstörer</option>}else{ <div></div>}
+      case 2 => <option id="z_klein" value="Zerstörer">Zerstörer</option>
        
-      case 5 => <option value="Zerstörer">Zerstörer</option><option value="U-Boot">U-Boot</option><option value="Kreuzer">Kreuzer</option>
+      case 5 => <option id="z_mittel" value="Zerstörer">Zerstörer</option><option id="u_mittel" value="U-Boot">U-Boot</option><option id="k_mittel" value="Kreuzer">Kreuzer</option>
         
-      case 10 => <option value="Zerstörer">Zerstörer 1</option><option value="Zerstörer">Zerstörer 2</option><option value="Zerstörer">Zerstörer 3</option><option value="Zerstörer">Zerstörer 4</option><option value="U-Boot">U-Boot 1</option><option value="U-Boot">U-Boot 2</option><option value="U-Boot">U-Boot 3</option><option value="Kreuzer">Kreuzer 1</option><option value="Kreuzer">Kreuzer 2</option><option value="Schlachtschiff">Schlachtschiff</option>
+      case 10 => <option id="z1" value="Zerstörer">Zerstörer 1</option><option id="z2" value="Zerstörer">Zerstörer 2</option><option id="z3" value="Zerstörer">Zerstörer 3</option><option id="z4" value="Zerstörer">Zerstörer 4</option><option id="u2" value="U-Boot">U-Boot 1</option><option id="u2" value="U-Boot">U-Boot 2</option><option id="u3" value="U-Boot">U-Boot 3</option><option id="k1" value="Kreuzer">Kreuzer 1</option><option id="k2" value="Kreuzer">Kreuzer 2</option><option id="s" value="Schlachtschiff">Schlachtschiff</option>
     }
     
     
--- Schiffe_09_11_2012_test/src/main/scala/code/snippet/SchiffSetzen.scala
+++ Schiffe_09_11_2012_test/src/main/scala/code/snippet/SchiffSetzen.scala
@@ -7,6 +7,12 @@ import schiffe.Schiffe
 import code.comet.SchiffeServer
 import javax.swing.JOptionPane
 import net.liftweb.http.js.JE.JsRaw
+import net.liftweb.http.js.JsCmds.SetHtml
+import net.liftweb.http.js.JsCmds.Replace
+import net.liftweb.http.js._
+import JsCmds._
+import net.liftweb.http.js.JSArtifacts
+import net.liftweb.http.js.JSArtifacts
 
 object SchiffSetzen {
  var laenge = 0  
@@ -26,19 +32,23 @@ object SchiffSetzen {
     if(typ=="set"){
       if(schiff=="Zerstörer"){
         Schiffe.controller.getSize match{
-          case 2 => SchiffAuswahl.z_klein = true
-          case 5 => SchiffAuswahl.z_mittel = true
+          case 2 =>{ SchiffAuswahl.z_klein = true; Replace(("z_klein"), <input type="hidden"/>)}
+          case 5 =>{ SchiffAuswahl.z_mittel = true; JsHideId("schifffSetzen")}
           case 10 => {
             if(SchiffAuswahl.z1==false){
               SchiffAuswahl.z1 = true
+            		  Replace("z1", <input type="hidden"/>)
             }else{
               if(SchiffAuswahl.z2==false){
                 SchiffAuswahl.z2=true
+                Replace(("z2"), <input type="hidden"/>)
               }else{
                 if(SchiffAuswahl.z3==false){
                   SchiffAuswahl.z3=true
+                  Replace(("z3"), <input type="hidden"/>)
                 }else{
                   SchiffAuswahl.z4=true
+                  Replace(("z4"), <input type="hidden"/>)
                 }
               }
             }
@@ -48,7 +58,7 @@ object SchiffSetzen {
       }else{
         if(schiff=="U-Boot"){
           Schiffe.controller.getSize match{
-          case 5 => SchiffAuswahl.u_mittel = true
+          case 5 => {SchiffAuswahl.u_mittel = true; Replace(("u_mittel"), <input type="hidden"/>)}
           case 10 => {
             if(SchiffAuswahl.u1==false){
               SchiffAuswahl.u1 = true
